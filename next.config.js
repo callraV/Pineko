e998d253545da61d675a786de74c6ea72a1867e6
CallRa <88369275+callraV@users.noreply.github.com>
2023-11-30 15:53:11 +0800

    Configure API #2

--- next.config.js
+++ next.config.js
@@ -9,7 +9,8 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:5328/api/:path*"
-            : "/api/",
+            : "http://127.0.0.1:5328/api/:path*",
+        // : "/api/",
       },
     ];
   },

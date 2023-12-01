6e6ef68688c0103a849695ec4d9721b3e578c301
CallRa <88369275+callraV@users.noreply.github.com>
2023-12-02 03:19:35 +0800

    Rewrite API #3 - deployment

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: "https://pineko-api.vercel.app/api//:path*",
+        source: "/api/:path*",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:5328/api/:path*"
